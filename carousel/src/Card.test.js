7a4e5cca6ca8976f8670e6ee2303b5d696cf0f79
Brandie Lucano <blucano.dev@gmail.com>
2024-01-16 16:29:17 -0800

    added skote test and snapshot test to Card and Carousel. added left arrow test in Carousel

---
+++ carousel/src/Card.test.js
@@ -0,0 +1,20 @@
+import { render, fireEvent } from "@testing-library/react";
+import Card from './Card.js';
+
+it("renders without crashing", function(){
+  render(<Card
+    caption="this is caption"
+    src='image1.jpg'
+    currNum={1}
+    totalNum={3} />)
+});
+
+// it("matches snapshot", function(){
+//   const { container } = render(
+//   <Card
+//     caption="this is caption"
+//     src='image1.jpg'
+//     currNum={1}
+//     totalNum={3} />)
+//     expect(container).toMatchSnapshot();
+// })
--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -2,6 +2,10 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
+it("renders without crashing", function(){
+  render(<Carousel photos={TEST_IMAGES} title="test title"/>)
+});
+
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
@@ -29,3 +33,43 @@ it("works when you click on the right arrow", function() {
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function(){
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  )
+  //move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+    //expect second image to show, not the first
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+
+    //move backwards(left) in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+    //expect first image to show, not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+ })
+
+// it("matches snapshot", function(){
+//   const { container } = render(
+//   <Carousel
+//     photos={TEST_IMAGES}
+//     title="test title"/>)
+//     expect(container).toMatchSnapshot();
+// })
